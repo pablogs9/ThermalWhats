c577e0504fb06ab1a7c9150d68caf504a249f889
pablogs9 <Pablo@MacBook-Pro-de-Pablo.local>
2014-11-19 22:12:36 +0100

    Update

--- LibThermal.py
+++ LibThermal.py
@@ -1,6 +1,7 @@
 import serial
 import time
 from math import ceil
+from math import floor
 from numpy import matrix
 from PIL import Image
 
@@ -16,7 +17,8 @@ class Thermal:
                                  timeout=0.1,
                                  xonxoff=0,
                                  rtscts=0,
-                                 interCharTimeout=0.04
+                                 dsrdtr=1,
+                                 interCharTimeout=0.01
                                  )
         self.ser.write([0x1B,0x52,0x07])
 
@@ -27,7 +29,8 @@ class Thermal:
     
     def writeBytes(self,bytes):
         self.ser.write(bytes)
-    
+        self.ser.flush()
+
     def emphatised(self,value):
         if value == 1:
             self.ser.write([0x1B,0x45,0x01])
@@ -114,38 +117,24 @@ class Thermal:
             self.ser.write([0x1B,0x42,0x00])
         self.ser.flush()
 
+    ## Separators 0x80 -> top
+    def separator(self,value):
+        self.ser.write([0x1D,0x2A,0x20,0x1])
+        self.ser.flush()
+        self.ser.write([value & 0xFF]*256)
+        self.ser.flush()
+        self.ser.write([0x1D,0x2F,0x03])
+        self.ser.flush()
+
+    def dotSeparator(self,value):
+        self.ser.write([0x1D,0x2A,0x20,0x1])
+        self.ser.flush()
+        self.ser.write([value & 0xFF,0x0]*128)
+        self.ser.flush()
+        self.ser.write([0x1D,0x2F,0x03])
+        self.ser.flush()
+
     def printBitmap(self,im,value):
-        w, h = im.size
-        if w > h:
-            im = im.crop(((w-h)/2,0,h+(w-h)/2,h))
-        elif h > w:
-            im = im.crop((0,(h-w)/2,w,w+(h-w)/2))
-        
-        im = im.resize((256,256))
-        
-        im = im.convert('1')
-        #im.show()
-	        
-        self.ser.write([0x1D,0x2A,0x20,0x20])
-        self.ser.flush()
-        for j in range(256):
-            for i in range(0,256,8):
-                data = 0
-                desp = 7
-                for p in range(8):
-                    if im.getpixel((j,p+i)) == 0:
-                        data |= 1 << desp
-                    desp = desp - 1
-                self.ser.write([data])
-                self.ser.flush()
-
-        if value == 1:
-            self.ser.write([0x1D,0x2F,0x03])
-        else:
-            self.ser.write([0x1D,0x2F,0x00])
-        self.ser.flush()
-
-    def printBitmap2(self,im,value):
         w, h = im.size
         if w > 256 and h > 256:
             if w > h:
@@ -156,15 +145,14 @@ class Thermal:
             im = im.resize((256,256))
         elif w > 256 or h > 256:
             im.thumbnail((256,256),Image.ANTIALIAS)
-    
+        
         im = im.convert('1')
         w, h = im.size
-        #im.show()
 
         self.ser.write([0x1D,0x2A,int(ceil(w/8.0)),int(ceil(h/8.0))])
         self.ser.flush()
-        for j in range(w):
-            for i in range(0,h,8):
+        for j in range(int(ceil(w/8.0))*8):
+            for i in range(0,int(ceil(h/8.0))*8,8):
                 data = 0
                 desp = 7
                 for p in range(8):
@@ -182,3 +170,85 @@ class Thermal:
         else:
             self.ser.write([0x1D,0x2F,0x00])
         self.ser.flush()
+
+    def printLargeBitmap(self,im):
+        w, h = im.size
+        if w > 256:
+            im = im.crop(((w-256)/2,0,256+(w-256)/2,h))
+        
+        im = im.convert('1')
+        w, h = im.size
+        
+        hAux = h
+        
+        k = 0
+        while hAux > 0:
+            if hAux > 256:
+                imaux = im.crop((0,k*256,w,k*256+256))
+                hAux = hAux - 256
+            #print "Corto e imprimo de " + str((0,k*256)) + " a " + str((w,k*256+256)) + " filas: 256"
+            else:
+                imaux = im.crop((0,k*256,w,k*256+hAux))
+                #print "Corto e imprimo de " + str((0,k*256)) + " a " + str((w,k*256+hAux)) + " filas: " + str(hAux)
+                hAux = 0
+            k = k + 1
+            self.printBitmap(imaux,1)
+            self.ser.flush()
+
+    def printCompleteBitmap(self,im):
+        im = im.convert('1')
+        w, h = im.size
+        #print "Imagen de " + str(w) + " por " + str(h)
+        im = im.crop((0,0,int(floor(w/256.0))*256,h))
+        
+    
+        w, h = im.size
+        #print "Imagen de " + str(w) + " por " + str(h)
+        wc = int(ceil(w/256.0))
+        hc = int(ceil(h/256.0))
+        #print "Tiras de papel: " + str(wc)
+        #print "Cuadrados por tira: " + str(hc)
+        
+        for i in range(wc):
+            hAux = h
+            self.textAling(1)
+            self.println("Tira " + str(i+1) + " de " + str(wc))
+            self.printLines(1)
+            self.textAling(0)
+            self.cutPaper()
+            for j in range(hc):
+                if hAux > 256:
+                    #print "Imprimiendo " + str(j) + "/" + str(i)
+                    #print str(i*256) + "," + str(j*256) + " " + str(i*256+256) + "," + str(j*256+256)
+                    imaux = im.crop((i*256,j*256,i*256+256,j*256+256))
+                    hAux = hAux - 256
+                else:
+                    #print "Imprimiendo " + str(j) + "/" + str(i)
+                    #print str(i*256) + "," + str(j*256) + " " + str(i*256+256) + "," + str(j*256+hAux)
+                    imaux = im.crop((i*256,j*256,i*256+256,j*256+hAux))
+                    hAux = 0
+                self.printBitmap(imaux,1)
+            self.printLines(4)
+            self.cutPaper()
+
+    def printiPhoneBitmap(self,im):
+        w, h = im.size
+        if w > 256:
+            im.thumbnail((256,h*int(ceil(256.0/w))),Image.ANTIALIAS)
+        im = im.convert('1')
+        w, h = im.size
+        
+        hAux = h
+        
+        k = 0
+        while hAux > 0:
+            if hAux > 256:
+                imaux = im.crop((0,k*256,w,k*256+256))
+                hAux = hAux - 256
+            #print "Corto e imprimo de " + str((0,k*256)) + " a " + str((w,k*256+256)) + " filas: 256"
+            else:
+                imaux = im.crop((0,k*256,w,k*256+hAux))
+                #print "Corto e imprimo de " + str((0,k*256)) + " a " + str((w,k*256+hAux)) + " filas: " + str(hAux)
+                hAux = 0
+            k = k + 1
+            self.printBitmap(imaux,1)
--- ThermalWhats.py
+++ ThermalWhats.py
@@ -1,19 +1,23 @@
 from Yowsup.connectionmanager import YowsupConnectionManager
 from Yowsup.Common.debugger import Debugger
 
-import threading,time, base64,os
+import threading,time, base64,os,sys
 import subprocess
 import urllib
 
 from LibThermal import *
 from PIL import Image
 
+timeref = -120
+printmode = 1
+
 ##Desconecting
 def ondisconnected(error):
-	methodsInterface.call("auth_login",("",base64.b64decode(""))) #Use here your whatsapp credentials
-	methodsInterface.call("ready")
-	methodsInterface.call("presence_sendAvailable")
-	print "Reconnecting"
+    time.sleep(2)
+    methodsInterface.call("auth_login",("34668882592",base64.b64decode("aeZDgEteVv4J4Oi5RLfI3FHZYbg=")))
+    methodsInterface.call("ready")
+    methodsInterface.call("presence_sendAvailable")
+    print "Reconnecting"
 
 ##Message delivered successfully
 def onMessageDelivered(jid, messageId):
@@ -25,11 +29,42 @@ def onAuthSuccess(username):
 
 ##Receive message and print it
 def onMessage(messageId,jid,content,timestamp,receiptRequested,name,b):
+    global timeref
+    global printmode
     methodsInterface.call("message_ack", (jid, messageId))
+
     if content == "END":
         t.printLines(2)
         t.cutPaper()
         return
+    
+    if content == "MODE1":
+        printmode = 1
+        methodsInterface.call("message_send", (jid, "Modo 1: impresion normal "))
+        return
+    elif content == "MODE2":
+        printmode = 2
+        methodsInterface.call("message_send", (jid, "Modo 2: impresion para iPhone"))
+        return
+    elif content == "MODE3":
+        printmode = 3
+        methodsInterface.call("message_send", (jid, "Modo 3: impresion larga"))
+        return
+    elif content == "MODE4":
+        printmode = 4
+        methodsInterface.call("message_send", (jid, "Modo 4: impresion completa (mucho gasto de papel)"))
+        return
+    
+    if time.time() - timeref > 120:
+        t.printLines(1)
+        t.smallFont(1)
+        t.textAling(1)
+        t.println(time.strftime("%d/%m/%Y %H:%M:%S"))
+        t.smallFont(0)
+        t.textAling(0)
+        timeref = time.time()
+        #t.dotSeparator(0x08)
+
     t.emphatised(1)
     t.printnln(name+ ": ")
     t.emphatised(0)
@@ -40,21 +75,44 @@ def onMessage(messageId,jid,content,timestamp,receiptRequested,name,b):
 ##Receive image and print it
 def onimage_received(messageId,jid,preview,url,size,receiptRequested,a):
     urllib.urlretrieve (url, messageId + ".jpg")
-    im = Image.open(messageId + ".jpg")
-    t.cutPaper()
-    t.printBitmap2(im,1)
-    t.printLines(4)
-    t.cutPaper()
-    methodsInterface.call("message_ack", (jid, messageId))
-    methodsInterface.call("message_send", (jid, "Foto imprimida. Gracias!"))
+    try:
+        im = Image.open(messageId + ".jpg")
+        #t.dotSeparator(0x08)
+        t.println("Imagen recibida a: " + time.strftime("%d/%m/%Y %H:%M:%S"))
+        t.println("Modo de impresion: " + str(printmode))
+        #t.dotSeparator(0x08)
+        t.cutPaper()
+        
+        if printmode == 1:
+            t.printBitmap(im,1)
+        elif printmode == 2:
+            t.printiPhoneBitmap(im)
+        elif printmode == 3:
+            t.printLargeBitmap(im)
+        elif printmode == 4:
+            t.printCompleteBitmap(im)
+
+
+        t.printLines(4)
+        t.cutPaper()
+        methodsInterface.call("message_send", (jid, "Foto imprimida. Gracias!"))
+    except:
+        methodsInterface.call("message_send", (jid, "Error en la foto!"))
+        print "Unexpected error:", sys.exc_info()[0]
+        raise
+    finally:
+        methodsInterface.call("message_ack", (jid, messageId))
 
 ##Audio receive and download
 def onaudio_received(messageId,jid,url,size,receiptRequested,a):
     urllib.urlretrieve (url, messageId + ".acc")
     methodsInterface.call("message_ack", (jid, messageId))
+    #avconv -i 1402571446-347.acc -ar 16000 out.flac
+    #curl -X POST -H 'Content-Type:audio/x-flac; rate=16000' -T out.flac 'https://www.google.com/speech-api/v2/recognize?lang=es&maxresults=10&pfilter=0&key=AIzaSyCGRH-QgpLZyHy0SlIIJS30oolFCUXk5S0'
+
 
 ##Init Yowsup and Thermal Printer
-t = Thermal("/dev/serial/by-id/usb-0d3a_0368-if00"); #Use here you own thermal printer mount point
+t = Thermal("/dev/serial/by-id/usb-0d3a_0368-if00")
 y = YowsupConnectionManager()
 y.setAutoPong(True)
 Debugger.enabled = False
@@ -70,7 +128,7 @@ signalsInterface.registerListener("audio_received", onaudio_received)
 signalsInterface.registerListener("receipt_messageDelivered", onMessageDelivered)
 
 ##Auth
-methodsInterface.call("auth_login", ("",base64.b64decode(""))) #Use here your whatsapp credentials
+methodsInterface.call("auth_login", ("34668882592",base64.b64decode("aeZDgEteVv4J4Oi5RLfI3FHZYbg=")))
 methodsInterface.call("ready")
 
 ##Profile photo and Status
@@ -80,4 +138,12 @@ methodsInterface.call("presence_sendAvailable")
 
 ##Infinite loop
 while True:
-    s = raw_input()
+    try:
+        s = raw_input()
+        t.println("Operador: " + s)
+    except KeyboardInterrupt:
+        print "Saliendo..."
+        t.cutPaper()
+        t.close()
+        sys.exit(0)
+
