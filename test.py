48d64145044e65c1d2b41ca56eba443f66e1f5dc
Pablo Garrido <pablogs9@gmail.com>
2015-12-31 10:47:53 +0100

    Update

--- test.py
+++ test.py
@@ -1,9 +1,6 @@
 from LibThermal import *
 t = Thermal("/dev/serial/by-id/usb-0d3a_0368-if00")
 t.println("Normal")
-t.separator(0x55)
-t.println("Dotted")
-t.dotSeparator(0x55)
 t.cutPaper()
 for i in range(256):
 	t.writeBytes([i])
